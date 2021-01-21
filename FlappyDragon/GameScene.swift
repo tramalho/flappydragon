2bec536c37045b4db56cab845ccb9bb7d830eac0
tramalho <thiagofacul@gmail.com>
2021-01-21 15:43:11 -0300

    162. Trabalhando com SKSpriteNodes

--- FlappyDragon/GameScene.swift
+++ FlappyDragon/GameScene.swift
@@ -11,11 +11,14 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var floor : SKSpriteNode!
+    private var intro : SKSpriteNode!
+    private let gameArea: CGFloat = 410.0
     
     override func didMove(to view: SKView) {
-
+        addBackground()
+        addFloor()
+        addIntro()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -24,5 +27,43 @@ class GameScene: SKScene {
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    private func addBackground() {
+        
+        let position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        
+        _ = createImageNode(zPosition: 0, imageName: "background", position: position)
+    }
+    
+    private func addFloor() {
+        
+        floor = SKSpriteNode(imageNamed: "floor")
+        
+        let position = CGPoint(x: self.size.width / 2, y: self.size.height - gameArea - floor.size.height / 2)
+        
+        floor.zPosition = 2
+        floor.position = position
+        
+        addChild(floor!)
+    }
+    
+    private func addIntro() {
+        
+        let position = CGPoint(x: self.size.width / 2, y:  self.size.height - 210)
+        
+        intro = createImageNode(zPosition: 0, imageName: "intro", position: position)
+    }
+    
+    private func createImageNode(zPosition: Int, imageName: String, position: CGPoint) -> SKSpriteNode {
+        
+        let skSpriteNode = SKSpriteNode(imageNamed: imageName)
+        
+        skSpriteNode.zPosition = CGFloat(zPosition)
+        skSpriteNode.position = position
+        
+        addChild(skSpriteNode)
+        
+        return skSpriteNode
     }
 }
