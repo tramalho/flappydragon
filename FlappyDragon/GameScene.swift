350111e8e82091d89c26e3458d4b06cac13856a2
tramalho <thiagofacul@gmail.com>
2021-01-21 16:44:43 -0300

    163. Animando nosso game com SKAction

--- FlappyDragon/GameScene.swift
+++ FlappyDragon/GameScene.swift
@@ -13,12 +13,17 @@ class GameScene: SKScene {
     
     private var floor : SKSpriteNode!
     private var intro : SKSpriteNode!
+    private var player : SKSpriteNode!
+    
     private let gameArea: CGFloat = 410.0
+    private let velocity = 100.0
     
     override func didMove(to view: SKView) {
         addBackground()
         addFloor()
         addIntro()
+        addPlayer()
+        addMoveFloor()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -40,7 +45,7 @@ class GameScene: SKScene {
         
         floor = SKSpriteNode(imageNamed: "floor")
         
-        let position = CGPoint(x: self.size.width / 2, y: self.size.height - gameArea - floor.size.height / 2)
+        let position = CGPoint(x: floor.size.width / 2, y: self.size.height - gameArea - floor.size.height / 2)
         
         floor.zPosition = 2
         floor.position = position
@@ -52,7 +57,39 @@ class GameScene: SKScene {
         
         let position = CGPoint(x: self.size.width / 2, y:  self.size.height - 210)
         
-        intro = createImageNode(zPosition: 0, imageName: "intro", position: position)
+        intro = createImageNode(zPosition: 3, imageName: "intro", position: position)
+    }
+    
+    private func addPlayer() {
+        
+        let position = CGPoint(x: 60, y: self.size.height - gameArea / 2)
+        
+        player = createImageNode(zPosition: 4, imageName: "player1", position: position)
+        
+        var textures: [SKTexture] = []
+        
+        for i in 1...4 {
+            textures.append(SKTexture(imageNamed: "player\(i)"))
+        }
+        
+        let animation = SKAction.animate(with: textures, timePerFrame: 0.09)
+        let repeatAction = SKAction.repeatForever(animation)
+        
+        player.run(repeatAction)
+    }
+    
+    
+    private func addMoveFloor() {
+        
+        let duration = Double(floor.size.width/2)/velocity
+        
+        let moveAction  = SKAction.moveBy(x: -(floor.size.width/2), y: 0, duration: duration)
+        let resetAction = SKAction.moveBy(x: floor.size.width/2, y: 0, duration: 0)
+
+        let sequenceAction = SKAction.sequence([moveAction, resetAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        
+        floor.run(repeatAction)
     }
     
     private func createImageNode(zPosition: Int, imageName: String, position: CGPoint) -> SKSpriteNode {
